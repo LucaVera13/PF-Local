c5cc62ffa5981412067578b5205e32a6583fe0a5
LucaVera13 <VeraLuuca@gmail.com>
2023-06-23 02:11:24 -0300

    aber
    aber

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,10 @@
 const app = require("./app");
-const connectDB = require("./Database/DB_connection")
+const connectDB = require("./Database/DB_connection");
 
-
-const PORT = 3001
+const PORT = 3001;
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server raised in port: http://localhost:${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server raised in port: http://localhost:${PORT}`);
   });
+});
