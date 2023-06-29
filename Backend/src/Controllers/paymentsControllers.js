46dbdc4f2736afb880dd914d0d2f27d0a9bc1c04
LucaVera13 <VeraLuuca@gmail.com>
2023-06-29 00:07:34 -0300

    2`13
    213

--- Backend/src/Controllers/paymentsControllers.js
+++ Backend/src/Controllers/paymentsControllers.js
@@ -4,21 +4,20 @@ const stripe = new Stripe(
 );
 const DOMAIN = "https://re-store-six.vercel.app/";
 
-const endpointSecret =
-  "whsec_602cd2598b4998749e3f929be11b474b1123a11e8d6a5c3bea2a9be9e5728679";
+// const endpointSecret =
+//   "whsec_602cd2598b4998749e3f929be11b474b1123a11e8d6a5c3bea2a9be9e5728679";
 
 const createSession = async (req, res) => {
-  const customer = await stripe.customers.create({
-    metadata: {
-      userId: req.body.userId,
-      carrito: JSON.stringify(req.body.cartItems),
-    },
-  });
-
   try {
+    const customer = await stripe.customers.create({
+      metadata: {
+        userId: req.body.userId,
+        // carrito: JSON.stringify(req.body.cartItems),
+      },
+    });
     const lineItems = req.body.cartItems.map((product) => {
       const { name, description, unit_amount, quantity, images } = product;
-
+      console.log(name, description, unit_amount, quantity, images);
       return {
         price_data: {
           product_data: {
@@ -43,29 +42,11 @@ const createSession = async (req, res) => {
 
     const paymentUrl = session.url;
     console.log(paymentUrl);
-    res.json({ url: paymentUrl });
+    res.redirect(303, session.url);
   } catch (error) {
     console.error("Error al crear la sesión de pago:", error);
     res.status(500).send("Error al crear la sesión de pago");
   }
 };
 
-const webHookController = async (req, res) => {
-  const payload = req.body;
-  const sig = req.headers["stripe-signature"];
-  try {
-    const event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object;
-      const line_items = await stripe.checkout.session.listLineItems(
-        session.id
-      );
-      console.log(line_items);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-module.exports = { createSession, webHookController };
+module.exports = { createSession };
